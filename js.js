9ba05bea72ea071f67a1d0c6e0464ccf8461f27c
Charles Bittner-Rossmiller <charles.bittnerrossmiller@gmail.com>
2015-09-09 12:49:59 -0500

    pseduocode for html, js, and css with initial ideas

---
+++ js.js
@@ -0,0 +1,34 @@
+
+// Board to play on 
+	// hover to have x or o appear when it's appropriate turn
+
+// turn based logic
+	// i++= even, then player 1's turn
+	// j++= odd, then player 2's turn
+	// loop it through so when even is true, player 1 goes and it's an x
+	// loop it through so when odd is true, player 2 goes and it's an o
+	
+	// player 1 multi picklist dropdown for teams 
+		// when dropdown is clicked, list 4 possible teams
+		// when team is clicked, change image
+	
+	// win counter
+		//Player 1 - ++ in the function to display a number when the win logic is triggered
+		//Player 2 - ++ in the function to display a number when the win logic is triggered
+	
+	// player 2 multi picklist dropdown for teams 
+		// when dropdown is clicked, list 4 possible teams
+		// when team is clicked, change image of logo underneath selector dropdowns
+	
+	// Rivalry Tracker bar chart based on % wins between player 1 and player 2 
+		// total games tracker function with a variable that ++ on every game, adjusts location of marker on rivalry chart with constantly updating variable where CSS % location is equal to the win %
+
+	// Win Logic
+		// i and j as variables that each get ++'ed to check against the board layout 
+		// if 1,2,3 vertical and horizontal then you can derive logic from the matchups of i&j
+		// 11,22,33 means diagonal victory for either i or j standing in as x and y on an axis
+
+
+	// Tie Logic
+		// Alert if you know cat is about to get the game
+
